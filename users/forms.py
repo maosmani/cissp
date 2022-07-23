48aef3c74306315049ab1f523b819179235c243d
Mohamed Amine Osmani <mohamed.amine.osmani.job@gmail.com>
2022-07-23 21:57:53 +0100

    first commit

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import NewUser
+
+class UserRegisterForm(UserCreationForm):
+    #email = forms.EmailField()
+
+    class Meta:
+        model = NewUser
+        #fields = ['user_name', 'email','first_name','last_name','country', 'password1', 'password2']
+        fields = ['user_name', 'email','first_name','last_name','country','about', 'password1', 'password2']
+
+        labels = {
+            "about": "Tell us little about yourself",
+        }
