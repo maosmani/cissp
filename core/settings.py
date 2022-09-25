e9bb321160415dc21c8223f5941fa237afca3134
Mohamed Amine Osmani <mohamed.amine.osmani.job@gmail.com>
2022-09-25 19:37:26 +0100

    h

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = '3gjm^_scxasa&ewe64tb$ass7t)z*a45aycsqnwo3j!+^=xq&j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cisspmeetings.tk']
+ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['cisspmeetings.tk']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 # Application definition
 
